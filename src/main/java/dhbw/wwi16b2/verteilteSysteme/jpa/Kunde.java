c7e45d34669373eff7f275f37df606dd0f7ab796
z003ne3b <z003ne3b@MD1K32DC.ad001.siemens.net>
2018-03-19 10:02:38 +0100

    Kunde Klasse

--- src/main/java/dhbw/wwi16b2/verteilteSysteme/jpa/Kunde.java
+++ src/main/java/dhbw/wwi16b2/verteilteSysteme/jpa/Kunde.java
@@ -1,14 +1,119 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dhbw.wwi16b2.verteilteSysteme.jpa;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 
 /**
  *
- * @author z003ne3b
+ * @author Steffen Lintz
  */
-public class Kunde {
+
+@Entity
+public class Kunde implements Serializable {
+    @Id
+    @GeneratedValue
+    private long id = 0;
     
+    @NotNull(message= "Vorname darf nicht leer sein")
+    private String vorname;
+    
+    @NotNull(message= "Nachname darf nicht leer sein")
+    private String nachname;
+    
+    @NotNull(message = "Straße darf nicht leer sein")
+    private String straße;
+    
+    @NotNull (message = "PLZ darf nicht leer sein")
+    private String plz;
+    
+    @NotNull (message = "Ort darf nicht leer sein")
+    private String ort;
+    
+    @NotNull (message = "Land darf nicht leer sein")
+    private String land;
+    
+//<editor-fold defaultstate="collapsed" desc="Konstruktoren">
+    public Kunde(String vorname, String nachname, String straße, String plz, String ort, String land, Leihvertrag leihvertrag) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.straße = straße;
+        this.plz = plz;
+        this.ort = ort;
+        this.land = land;
+        this.leihvertrag = leihvertrag;
+    }
+    
+    public Kunde() {
+    }
+//</editor-fold>
+    
+//<editor-fold defaultstate="collapsed" desc="Getter and Setters">
+    public long getId() {
+        return id;
+    }
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public String getVorname() {
+        return vorname;
+    }
+    
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+    
+    public String getNachname() {
+        return nachname;
+    }
+    
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+    
+    public String getStraße() {
+        return straße;
+    }
+    
+    public void setStraße(String straße) {
+        this.straße = straße;
+    }
+    
+    public String getPlz() {
+        return plz;
+    }
+    
+    public void setPlz(String plz) {
+        this.plz = plz;
+    }
+    
+    public String getOrt() {
+        return ort;
+    }
+    
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+    
+    public String getLand() {
+        return land;
+    }
+    
+    public void setLand(String land) {
+        this.land = land;
+    }
+    
+    public Leihvertrag getLeihvertrag() {
+        return leihvertrag;
+    }
+    
+    public void setLeihvertrag(Leihvertrag leihvertrag) {
+        this.leihvertrag = leihvertrag;
+    }
+//</editor-fold>
 }
