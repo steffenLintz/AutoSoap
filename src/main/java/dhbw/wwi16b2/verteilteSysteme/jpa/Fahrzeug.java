ae1127d8538ef0d4e06c3f8888c96559a16f4c24
z003ne3b <z003ne3b@MD1K32DC.ad001.siemens.net>
2018-03-19 10:28:42 +0100

    Fahrzeug und Leihvertrag

--- src/main/java/dhbw/wwi16b2/verteilteSysteme/jpa/Fahrzeug.java
+++ src/main/java/dhbw/wwi16b2/verteilteSysteme/jpa/Fahrzeug.java
@@ -5,10 +5,75 @@
  */
 package dhbw.wwi16b2.verteilteSysteme.jpa;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 /**
  *
  * @author z003ne3b
  */
-public class Fahrzeug {
+@Entity
+public class Fahrzeug implements Serializable {
+    @Id
+    @GeneratedValue
+    private long id = 0;
+    
+    @NotNull(message = "Hersteller darf nicht null sein")
+    private String hersteller;
+    
+    @NotNull(message = "Modell darf nicht null sein")
+    private String Modell;
+    
+    @NotNull(message = "Baujahr darf nicht null sein")
+    private int baujahr;
+
+   //<editor-fold defaultstate="collapsed" desc="Konstruktoren">
+    public Fahrzeug(String hersteller, String Modell, int baujahr) {
+        this.hersteller = hersteller;
+        this.Modell = Modell;
+        this.baujahr = baujahr;
+    }
+    
+    public Fahrzeug() {
+    }
+    
+//</editor-fold>
+    
+   //<editor-fold defaultstate="collapsed" desc="Getter and Setter">
+    public long getId() {
+        return id;
+    }
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public String getHersteller() {
+        return hersteller;
+    }
+    
+    public void setHersteller(String hersteller) {
+        this.hersteller = hersteller;
+    }
+    
+    public String getModell() {
+        return Modell;
+    }
+    
+    public void setModell(String Modell) {
+        this.Modell = Modell;
+    }
+    
+    public int getBaujahr() {
+        return baujahr;
+    }
+    
+    public void setBaujahr(int baujahr) {
+        this.baujahr = baujahr;
+    }
+//</editor-fold>
     
 }
--- src/main/java/dhbw/wwi16b2/verteilteSysteme/jpa/Leihvertrag.java
+++ src/main/java/dhbw/wwi16b2/verteilteSysteme/jpa/Leihvertrag.java
@@ -5,10 +5,91 @@
  */
 package dhbw.wwi16b2.verteilteSysteme.jpa;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.validation.constraints.NotNull;
+
 /**
  *
  * @author z003ne3b
  */
-public class Leihvertrag {
+@Entity
+public class Leihvertrag implements Serializable {
     
+    @ManyToOne
+    Kunde kunde;
+    
+    @ManyToOne
+    Fahrzeug fahrzeug;
+    
+    @Id
+    @GeneratedValue
+    private final long id = 0;
+    
+    @NotNull(message="KundenID -Fehler")
+    private final long KundenId = kunde.getId();
+    
+    
+    @NotNull(message="FahrzeugID -Fehler")
+    private final long fahrzeugId = fahrzeug.getId();
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date beginndatum;
+    
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date enddatum;
+    
+    
+//<editor-fold defaultstate="collapsed" desc="Konstruktoren">
+    public Leihvertrag(Kunde kunde, Fahrzeug fahrzeug, Date beginndatum, Date enddatum) {
+        this.kunde = kunde;
+        this.fahrzeug = fahrzeug;
+        this.beginndatum = beginndatum;
+        this.enddatum = enddatum;
+    }
+    
+    public Leihvertrag() {
+    }
+    
+//</editor-fold>
+ 
+//<editor-fold defaultstate="collapsed" desc="Getter and Setter">
+    public Kunde getKunde() {
+        return kunde;
+    }
+    
+    public void setKunde(Kunde kunde) {
+        this.kunde = kunde;
+    }
+    
+    public Fahrzeug getFahrzeug() {
+        return fahrzeug;
+    }
+    
+    public void setFahrzeug(Fahrzeug fahrzeug) {
+        this.fahrzeug = fahrzeug;
+    }
+    
+    public Date getBeginndatum() {
+        return beginndatum;
+    }
+    
+    public void setBeginndatum(Date beginndatum) {
+        this.beginndatum = beginndatum;
+    }
+    
+    public Date getEnddatum() {
+        return enddatum;
+    }
+    
+    public void setEnddatum(Date enddatum) {
+        this.enddatum = enddatum;
+    }
+//</editor-fold>
+  
 }
